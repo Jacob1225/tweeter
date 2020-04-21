a60a27a3baeed6963d3be3647b86598e52c46f38
Jacob1225 <carlone.jacob@gmail.com>
2020-04-21 20:52:33 +0000

    Added features so the word count updates as the user types and turns red when they have exceeded the max length allowed using the keyUp()

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    
+      $("textarea#tweet-text").keyup(function() {
+        maxLength = 140;
+        currentLength = this.value.length
+        if (currentLength > maxLength) {
+          $("output.counter").addClass("red");
+        } else {
+            $("output.counter").removeClass("red");
+        }
+        $("output.counter").val(maxLength - currentLength);
+      });
+  });
